d0c574f38fd0003476f0451fb41b9ebeac38b553
Akshit Vadodariya <108290138+AkshitVadodariya1201@users.noreply.github.com>
2024-06-26 23:01:10 +0530

    changing-title-dynamically-adding-favicons-to-textutils (#4)
    * "Updated index.html and Navbar.js files: changed meta description and title, and modified toggle mode label text."
    
    * feat: Add dynamic page title based on selected mode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ export default function Navbar(props) {
           </form> */}
           <div className={`form-check form-switch text-${props.mode==="light"?"dark":"light"}`}>
             <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault" />
-              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable {props.mode==="light"?"dark":"light"} Mode</label>
           </div>
         </div>
       </div>
