75f9ea78b2b63955859ed57789b5693f9a0b0e1b
Akshit <axitvadodariya264@gmail.com>
2024-07-15 16:44:38 +0530

    feat: Add react-router-dom for routing functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { useState } from 'react';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -37,11 +43,17 @@ function App() {
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About" /> */}
-      <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />
-      </div>
+
+      <Router>
+        <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />} />
+          </Routes>
+        </div>
+      </Router>
       {/* <About/> */}
     </>
   );
