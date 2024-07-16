3054c6b5bd96734c574d92b9868b2297dd4642c2
Akshit Vadodariya <108290138+AkshitVadodariya1201@users.noreply.github.com>
2024-07-17 00:09:28 +0530

    feat: Improve alert component and add dynamic page title (#8)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import "./App.css";
-// import About from "./components/About";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -26,38 +26,38 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enabled", "success");
-      document.title = "TextUtils - Dark Mode";
+      // document.title = "TextUtils - Dark Mode";
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
-      document.title = "TextUtils - Light Mode";
+      // document.title = "TextUtils - Light Mode";
     }
   };
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About" /> */}
 
-      {/* <Router> */}
+      <Router>
         <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container my-3">
-          {/* <Routes>
-            <Route exact path="/about" element={<About />} />
+          <Routes>
+            <Route exact path="/about" element={<About mode={mode} />} />
             <Route
               exact
               path="/"
-              element={ */}
+              element={
                 <TextForm
-                  heading="Enter the text to analyze below"
+                  heading="Try TextUtils - word counter, character counter, remove extra spaces"
                   mode={mode}
                   showAlert={showAlert}
                 />
-              {/* }
+              }
             />
-          </Routes> */}
+          </Routes>
         </div>
-      {/* </Router> */}
+      </Router>
       {/* <About/> */}
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -17,7 +17,7 @@ export default function TextForm(props) {
   }
 
   const heandleClearClick = () => {
-    let newText = " ";
+    let newText = "";
     setText(newText)
     props.showAlert("Text Clear! ", "success")
   }
@@ -25,10 +25,11 @@ export default function TextForm(props) {
   const heandleCopy = () => {
     let text = document.getElementById("myBox");
     text.select();
-    navigator.clipboard.writeText(text.value)
+    navigator.clipboard.writeText(text.value);
+    document.getSelection().removeAllRanges();
     props.showAlert("Copied to Clipboard! ", "success")
   }
-  
+
   const heandleExtraSpaces = () => {
     let newText = text.split(/[ ]+/);
     setText(newText.join(" "))
@@ -41,26 +42,26 @@ export default function TextForm(props) {
   return (
     <>
       <div className='container' style={{ color: props.mode === "light" ? "black" : "white" }}>
-        <h1>{props.heading}</h1>
+        <h1 className='mb-4'>{props.heading}</h1>
         <div className="mb-3">
           <textarea className="form-control" value={text} style={{
-            backgroundColor: props.mode === "light" ? "white" : "grey", color: props.mode === "light" ? "black" : "white"
+            backgroundColor: props.mode === "light" ? "white" : "#13466e", color: props.mode === "light" ? "black" : "white"
           }} onChange={heandleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className='btn btn-primary mx-1' onClick={heandleUpClick}>Convert to Uppercase</button>
-        <button className='btn btn-primary mx-1' onClick={heandleLoClick}>Convert to Lowercase</button>
-        <button className='btn btn-primary mx-1' onClick={heandleClearClick}>Cleare Text</button>
-        <button className='btn btn-primary mx-1' onClick={heandleCopy}>Copy Text</button>
-        <button className='btn btn-primary mx-1' onClick={heandleExtraSpaces}>Remove Extra Spaces</button>
+        <button disabled={text.length === 0} className='btn btn-primary mx-1 my-1' onClick={heandleUpClick}>Convert to Uppercase</button>
+        <button disabled={text.length === 0} className='btn btn-primary mx-1 my-1' onClick={heandleLoClick}>Convert to Lowercase</button>
+        <button disabled={text.length === 0} className='btn btn-primary mx-1 my-1' onClick={heandleClearClick}>Cleare Text</button>
+        <button disabled={text.length === 0} className='btn btn-primary mx-1 my-1' onClick={heandleCopy}>Copy Text</button>
+        <button disabled={text.length === 0} className='btn btn-primary mx-1 my-1' onClick={heandleExtraSpaces}>Remove Extra Spaces</button>
       </div>
 
       <div className={`container my-3 text-${props.mode === "light" ? "dark" : "light"}`}>
         <h2>Your text summary</h2>
-        <p>{text.split(" ").length} words and {text.length} charactes</p>
-        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <p>{text.split(" ").filter((element) => element.length !== 0).length} words and {text.length} charactes</p>
+        <p>{0.008 * text.split(" ").filter((element) => element.length !== 0).length} Minutes read</p>
 
         <h2>Preview</h2>
-        <p>{text.length > 0 ? text : "Enter something in the textbox above to preview it here"}</p>
+        <p>{text.length > 0 ? text : "Nothing to preview!"}</p>
       </div>
     </>
   )
