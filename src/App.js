febb8da0dcbb603505243bb425029a4bf552983c
Akshit <axitvadodariya264@gmail.com>
2024-07-16 18:03:17 +0530

    chore: Update package name to TextUtils-React

--- src/App.js
+++ src/App.js
@@ -1,59 +1,63 @@
-import { useState } from 'react';
-import './App.css';
-import About from './components/About';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  // Link
-} from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+// import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light');
+  const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
+      type: type,
     });
 
     setTimeout(() => {
-      setAlert(null)
+      setAlert(null);
     }, 1500);
-  }
+  };
 
   const toggleMode = () => {
-    if (mode === 'light') {
-      setMode('dark');
-      document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled", "success")
-      document.title = "TextUtils - Dark Mode"
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+      document.title = "TextUtils - Light Mode";
     }
-    else {
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled", "success")
-      document.title = "TextUtils - Light Mode"
-    }
-  }
+  };
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About" /> */}
 
-      <Router>
-        <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode} />
+      {/* <Router> */}
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container my-3">
-          <Routes>
+          {/* <Routes>
             <Route exact path="/about" element={<About />} />
-            <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />} />
-          </Routes>
+            <Route
+              exact
+              path="/"
+              element={ */}
+                <TextForm
+                  heading="Enter the text to analyze below"
+                  mode={mode}
+                  showAlert={showAlert}
+                />
+              {/* }
+            />
+          </Routes> */}
         </div>
-      </Router>
+      {/* </Router> */}
       {/* <About/> */}
     </>
   );
